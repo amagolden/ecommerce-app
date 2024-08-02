9727cb0fb6b950e9b2973981c15d3c21000b229b
amagolden <amagoldenstein@gmail.com>
2024-08-02 16:50:16 -0400

    fetched Google Books api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [books, setBooks] = useState([]);
+
+  const fetchData = () => {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=reese-book-club')
+    .then(res => res.json())
+    .then(data => setBooks(data.items))
+  }
+
+useEffect(() => {
+  fetchData();
+}, [])
+
+console.log(books.map(book => book.volumeInfo.title))
+console.log(books.map(book => book.volumeInfo.authors))
+console.log(books.map(book => book.volumeInfo.publishedDate))
+console.log(books.map(book => book.volumeInfo.imageLinks.thumbnail))
+
   return (
     <div className="App">
-      <header className="App-header">
         <h1>HELLOOOO</h1>
-      </header>
     </div>
   );
 }
