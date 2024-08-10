743e6a2622c314dd3a9c687d90e68b995a685418
amagolden <amagoldenstein@gmail.com>
2024-08-09 23:06:26 -0400

    added purchase button

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -2,6 +2,7 @@ const BookCard = ({ book, handleAddToCart }) => {
     const { key, title, author_name, isbn, cover_i, cover_edition_key, id_amazon, ratings_average, isInCart } = book;
 
     const image_src = `https://covers.openlibrary.org/b/olid/${cover_edition_key}-M.jpg`;
+    const amazonLink = `https://amazon.com/dp/${id_amazon}`;
 
     return (
         <div className='book'>
@@ -12,6 +13,7 @@ const BookCard = ({ book, handleAddToCart }) => {
             <button onClick={() => handleAddToCart(key)}>
                 {isInCart ? "Remove from Cart" : "Add to Cart"}
             </button>
+            <a href={amazonLink}><button onClick={amazonLink}>Buy on Amazon</button></a>
         </div>
 )}
 
